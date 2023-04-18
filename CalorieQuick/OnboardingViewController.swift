a232fe65f80ad56d69a02e9e611554112458f08a
Vladimir Lukyanenko <>
2023-04-18 21:59:22 +0300

    first commit

---
+++ CalorieQuick/OnboardingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingViewController.swift
+//  CalorieQuick
+//
+//  Created by Vladimir Lukyanenko on 18.04.2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
